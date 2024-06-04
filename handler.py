a296276ecdfc79272f1f3b29ae6fc9da1cd6cfe0
nalbam <me@nalbam.com>
2024-06-04 15:46:15 +0900

    add bedrock:InvokeModel

--- handler.py
+++ handler.py
@@ -19,10 +19,8 @@ SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 DYNAMODB_TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME", "gureumi-ai-bot-context")
 
 # Amazon Bedrock Model ID
-TEXT_MODEL_ID = os.environ.get(
-    "TEXT_MODEL_ID", "anthropic.claude-3-sonnet-20240229-v1:0"
-)
-IMAGE_MODEL_ID = os.environ.get("IMAGE_MODEL_ID", "stability.stable-diffusion-xl-v1")
+TEXT_MODEL_ID = os.environ.get("TEXT_MODEL_ID", "anthropic.claude-3")
+IMAGE_MODEL_ID = os.environ.get("IMAGE_MODEL_ID", "stability.stable-diffusion-xl")
 
 ANTHROPIC_VERSION = os.environ.get("ANTHROPIC_VERSION", "bedrock-2023-05-31")
 ANTHROPIC_TOKENS = int(os.environ.get("ANTHROPIC_TOKENS", 1024))
