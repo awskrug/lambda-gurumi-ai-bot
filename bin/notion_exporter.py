b1b5bfbaef16dba726d4ccfe2811ebf50836e05b
nalbam <me@nalbam.com>
2024-08-22 11:01:08 +0900

    chore: Update sync-notion and start-ingestion workflows

--- bin/notion_exporter.py
+++ bin/notion_exporter.py
@@ -3,16 +3,16 @@ import os
 from python_notion_exporter import NotionExporter, ExportType, ViewExportType
 
 
-NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+NOTION_TOKEN_V2 = os.getenv("NOTION_TOKEN_V2")
 NOTION_FILE_TOKEN = os.getenv("NOTION_FILE_TOKEN")
 
-NOTION_PAGE_ID = os.getenv("NOTION_PAGE_ID", "0c7c08203a9b4435a4ca07b6454151d7")
 NOTION_PAGE_NAME = os.getenv("NOTION_PAGE_NAME", "demo")
+NOTION_PAGE_ID = os.getenv("NOTION_PAGE_ID", "7aace0412a82431996f61a29225a95ec")
 
 
 if __name__ == "__main__":
     exporter = NotionExporter(
-        token_v2=NOTION_TOKEN,
+        token_v2=NOTION_TOKEN_V2,
         file_token=NOTION_FILE_TOKEN,
         pages={NOTION_PAGE_NAME: NOTION_PAGE_ID},
         export_directory="build",
