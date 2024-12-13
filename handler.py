4b1b8ea6683240e47936bd37c446c437c8ccb0e3
nalbam <me@nalbam.com>
2024-12-13 10:14:32 +0900

    프롬프트 관련 주석을 제거하고 코드 정리

--- handler.py
+++ handler.py
@@ -408,20 +408,15 @@ def conversation(say: Say, thread_ts, query, channel, client_msg_id):
     latest_ts = result["ts"]
 
     prompts = []
-    # prompts.append("User: {}".format(PERSONAL_MESSAGE))
-    # prompts.append(
-    #     "If you don't know the answer, just say that you don't know, don't try to make up an answer."
-    # )
+    prompts.append("User: {}".format(PERSONAL_MESSAGE))
+    prompts.append(
+        "If you don't know the answer, just say that you don't know, don't try to make up an answer."
+    )
 
-    # if SYSTEM_MESSAGE != "None":
-    #     prompts.append(SYSTEM_MESSAGE)
+    if SYSTEM_MESSAGE != "None":
+        prompts.append(SYSTEM_MESSAGE)
 
-    # prompts.append("<question> 태그로 감싸진 질문에 답변을 제공하세요.")
-
-    # tz = timezone(timedelta(hours=9))
-    # now = datetime.now(tz)
-
-    # prompts.append("<now>{}</now>".format(now.isoformat()))
+    prompts.append("<question> 태그로 감싸진 질문에 답변을 제공하세요.")
 
     try:
         # # Get the knowledge base contexts
@@ -444,21 +439,21 @@ def conversation(say: Say, thread_ts, query, channel, client_msg_id):
 
             contexts = conversations_replies(channel, thread_ts, client_msg_id)
 
-            # prompts.append(
-            #     "<history> 에 정보가 제공 되면, 대화 기록을 참고하여 답변해 주세요."
-            # )
+            prompts.append(
+                "<history> 에 정보가 제공 되면, 대화 기록을 참고하여 답변해 주세요."
+            )
             prompts.append("<history>")
             prompts.append("\n\n".join(contexts))
             prompts.append("</history>")
 
         # Add the question to the prompts
-        # prompts.append("")
+        prompts.append("")
         prompts.append("<question>")
         prompts.append(query)
         prompts.append("</question>")
-        # prompts.append("")
+        prompts.append("")
 
-        # prompts.append("Assistant:")
+        prompts.append("Assistant:")
 
         # Combine the prompts
         prompt = "\n".join(prompts)
